99ced78f189e86dd4de5d4f88a5212ae3d409b13
XBRAGI <kateoleynik2002@mail.ru>
2023-02-15 21:30:52 +0300

    Finish_calc

--- cody.js
+++ cody.js
@@ -5,28 +5,35 @@ const plus = document.getElementById('plus')
 const minus = document.getElementById('minus')
 const multiply = document.getElementById('multiply')
 const division = document.getElementById('division')
+const input_result = document.getElementById('input_result')
+
 
 plus.onclick = function(){
 	let result = Number(input_first.value) + Number(input_second.value)
-	input_first.value = result
+	input_result.value = result
 	input_second.value = ''
+	input_first.value = ''
 }
 
 minus.onclick = function(){
 	let result = Number(input_first.value) - Number(input_second.value)
-	input_first.value = result
+	input_result.value = result
 	input_second.value = ''
+	input_first.value = ''
 }
 
 multiply.onclick = function(){
 	let result = Number(input_first.value) * Number(input_second.value)
-	input_first.value = result
+	input_result.value = result
 	input_second.value = ''
+	input_first.value = ''
 }
 
 division.onclick = function(){
 	let result = Number(input_first.value) / Number(input_second.value)
-	input_first.value = result
+	input_result.value = result
 	input_second.value = ''
+	input_first.value = ''
 }
+
 
